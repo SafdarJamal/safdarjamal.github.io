ec473a910be6f3ca511418c9a9efb867e6261a70
Safdar Jamal <safdar.dev@gmail.com>
2019-05-22 00:12:25 +0500

    Import dependencies through modules

---
+++ source/scripts/index.js
@@ -0,0 +1,4 @@
+// Bootstrap Dependencies
+import '../libraries/bootstrap-4.3.1/js/jquery-3.4.1.slim.min.js';
+import '../libraries/bootstrap-4.3.1/js/bootstrap.bundle.min.js';
+import '../libraries/theme/js/theme.js';
