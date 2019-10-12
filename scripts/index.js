40561d595a63c2daf7bfb2373617f6d32207f55d
Safdar Jamal <safdar.dev@gmail.com>
2019-10-12 15:41:09 +0500

    Update libraries import

--- scripts/index.js
+++
@@ -1,4 +0,0 @@
-// Bootstrap Dependencies
-import '../libraries/bootstrap-4.3.1/js/jquery-3.4.1.slim.min.js';
-import '../libraries/bootstrap-4.3.1/js/bootstrap.bundle.min.js';
-import '../libraries/theme/js/theme.js';
